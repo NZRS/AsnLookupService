9ba7b0c7c14af51e09d2811b5225221ab2ca3420
Sebastian Castro <sebastian@nzrs.net.nz>
2015-03-25 15:33:16 +1300

    Reorganize code and add setup.py to make it installable using standard python tools.

---
+++ setup.py
@@ -0,0 +1,13 @@
+__author__ = 'secastro'
+
+from setuptools import setup
+
+setup(name='asn_name_lookup',
+      version='0.1',
+      description="Provides a short and long description for an AS number using Team Cymru lookup service",
+      author="Sebastian Castro",
+      author_email="sebastian@nzrs.net.nz",
+      license="GPL3",
+      packages=['asn_name_lookup'],
+      zip_safe=False)
+
